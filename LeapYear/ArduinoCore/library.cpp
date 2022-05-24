907d109d298b7a47b5dfdcccbe9bfa1762876575
Jasmin <jasminnielsen97@gmail.com>
2022-05-24 10:57:53 +0200

    Initial commit.

---
+++ LeapYear/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 24/05/2022 09.19.36
+ * Author : Jasmin
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
---
+++ LeapYear/BareMinimum/main.cpp
@@ -0,0 +1,39 @@
+﻿#include <Arduino.h>
+
+/* Pseudo code:	
+	Step 1 -> Save an integer named YEAR and assign it a value of the current year.
+	Step 2 -> Make an IF statement which checks if the YEAR can be divided(/) with 4 AND NOT (%) with 100.
+	Step 3 -> OR Check in the same IF statement if it can be divided(/) with 400.
+	Step 4 -> If above are TRUE then PRINT 'Year is a leap year!'.
+	Step 5 -> If above is FALSE then PRINT 'Year is not a leap year!'.
+*/
+const int pinForDiode = 2;
+const int year = 1600;
+
+unsigned long previousMillis = 0;
+const long interval = 1000;
+
+
+void setup() {
+  // put your setup code here, to run once:
+ Serial.begin(9600);
+ pinMode(pinForDiode, OUTPUT);
+}
+
+void loop() {
+	unsigned long currentMillis = millis();
+	if (currentMillis - previousMillis >= interval)
+	{
+		if ((year % 4 == 0) && (year % 100 != 0) || (year % 400  == 0))
+		{
+			digitalWrite(pinForDiode, HIGH);
+			Serial.println("Your year is a leap year!");
+		}
+		else
+		{
+			digitalWrite(pinForDiode, LOW);
+			Serial.println("Your year is NOT a leap year.");
+		}
+		previousMillis = currentMillis;
+	}
+}
